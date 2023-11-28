d31987722e913f4d8298b8cbabd81a11677b5b7c
luisggf <luis.guigfonseca@gmail.com>
2023-11-28 02:47:46 -0300

    generate valid pathways done

--- travelling_salesman.py
+++ travelling_salesman.py
@@ -7,7 +7,7 @@ import csv
 import random
 NUM_CITIES = 4
 POPULATION_SIZE = 10
-MIXING_NUMBER = 2
+MIXING_NUMBER = 4
 MUTATION_RATE = 0.01
 
 
@@ -15,14 +15,19 @@ MUTATION_RATE = 0.01
 def fitness_score(seq, distance_matrix):
     total_distance = 0
     try:
+        # sequencia para avaliação invalida
+        if seq[0] != seq[-1]:
+            return None
+
         for i in range(NUM_CITIES - 1):
             city1 = seq[i]
             city2 = seq[i+1]
             if city2 in distance_matrix[city1]:
                 distance_between_cities = distance_matrix[city1][city2]
                 total_distance += distance_between_cities
+            # se dada cidade nao tiver caminho pra cidade de destino a sequencia é invalida
             else:
-                continue
+                return None
         # total_distance += distance_matrix[seq[-1]][seq[0]]
         return total_distance
     except Exception as error:
@@ -87,12 +92,12 @@ def crossover(parents):
 # Create the routine to mutate a solution
 # A operator to create diversity in the population
 def mutate(seq, cities):
-    for row in range(len(seq)):
+    for row in range(NUM_CITIES - 1):
         if random.random() < MUTATION_RATE:
             # Escolhe uma cidade aleatória do conjunto
             new_city = random.choice(cities)
-            seq[row] = new_city
-
+            if new_city not in seq:
+                seq[row] = new_city
     return seq
 
 # Print the solution
@@ -103,7 +108,7 @@ def print_found_goal(population, distance_matrix, to_print=True):
         score = fitness_score(ind, distance_matrix)
         if to_print:
             print(f'{ind}. Score: {score}')
-        if score == sc.comb(NUM_CITIES, 2):
+        if score == sc.comb(NUM_CITIES, 4):
             if to_print:
                 print('Solution found')
             return True
@@ -137,14 +142,11 @@ def evolution(population, distance_matrix, fitness_score, cities):
 
 generation = 0
 
-# Create the initial population (solutions)
 
-
-def generate_population(graph):
+def generate_population():
     population = []
     cities = ['Ouro Preto', 'Mariana', 'Alvinopolis', 'BH']
 
-    # Criar uma matriz de distâncias entre as cidades
     distance_matrix = {
         'Ouro Preto': {'Mariana': 14.4, 'BH': 101.5},
         'Mariana': {'Ouro Preto': 14.4, 'Alvinopolis': 69.3},
@@ -152,12 +154,56 @@ def generate_population(graph):
         'BH': {'Ouro Preto': 69.3, 'Alvinopolis': 162}
     }
 
-    for i in range(POPULATION_SIZE):
-        # Criar uma rota aleatória inicial
-        route = random.sample(cities, len(cities))
-        population.append(route)
+    for _ in range(POPULATION_SIZE):
+        valid_route = []
+        remaining_cities = cities.copy()
+        current_city = random.choice(remaining_cities)
+
+        while remaining_cities:
+            if current_city not in valid_route:
+                valid_route.append(current_city)
+                remaining_cities.remove(current_city)
+
+            possible_cities = [
+                city for city in remaining_cities if city in distance_matrix[current_city]]
+
+            if not possible_cities:
+                break
+
+            next_city = random.choice(possible_cities)
+            current_city = next_city
+
+        if valid_route[-1] != valid_route[0] and valid_route[-1] in distance_matrix.get(valid_route[0], {}):
+            valid_route.append(valid_route[0])
+            population.append(valid_route)
 
     return population, distance_matrix
+
+
+# def generate_population(graph, cities):
+#     population = []
+
+#     # Criar uma matriz de distâncias entre as cidades
+#     distance_matrix = {
+#         'João Monlevade': {'Ipatinga': 106, 'Belo Horizonte': 117, 'Capelinha': 359, 'Visc. Do Rio Branco': 213},
+#         'Ipatinga': {'Gov. Valadares': 106, 'João Monlevade': 106},
+#         'Gov. Valadares': {'Capelinha': 214, 'Ipatinga': 106},
+#         'Capelinha': {'Montes Claros': 317, 'João Monlevade': 359, 'Gov. Valadares': 214},
+#         'Montes Claros': {'Belo Horizonte': 420, 'Capelinha': 317, 'Araxá': 566},
+#         'Belo Horizonte': {'João Monlevade': 117, 'Montes Claros': 420, 'Araxá': 362, 'Barbacena': 117},
+#         'Barbacena': {'Juiz de Fora': 101, 'Belo Horizonte': 117, 'Araxá': 506, },
+#         'Juiz de Fora': {'Barbacena': 101, 'Visc. Do Rio Branco': 128},
+#         'Visc. Do Rio Branco': {'João Monlevade': 213, 'Juiz de Fora': 128},
+#         'Araxá': {'Belo Horizonte': 362, 'Barbacena': 506, 'Montes Claros': 566}
+
+#     }
+
+#     for i in range(POPULATION_SIZE):
+#         # Criar uma rota aleatória inicial
+#         route = random.sample(cities, len(cities))
+#         population.append(route)
+
+#     return population, distance_matrix
 
 
 def save_to_csv(generation, score, time, num_pop, mutation_rate, n_queens):
@@ -171,6 +217,10 @@ def save_to_csv(generation, score, time, num_pop, mutation_rate, n_queens):
 
 
 cities_graph = Weighted_Graph()
+
+# cities = ['Montes Claros', 'Capelinha', 'Gov. Valadares', 'Ipatinga', 'João Monlevade',
+#           'Visc. Do Rio Branco', 'Juiz de Fora', 'Barbacena', 'Belo Horizonte', 'Araxá',]
+
 cities = ['Ouro Preto', 'Mariana', 'Alvinopolis', 'BH']
 
 # Generate Random Population
